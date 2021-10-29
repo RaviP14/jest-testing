05c9710c93d4cd4a8894434f35cba310e418adea
RaviP14 <ravi.patel14@outlook.com>
2021-10-29 14:42:17 +0100

    Added more tests & code for calculator.

--- code/calculator.js
+++ code/calculator.js
@@ -1,6 +1,15 @@
 function calculator(num1, num2) {
     this.add = () => {
-        return num1 + num2
+        return num1 + num2;
+    }
+    this.subtract = () => {
+        return num1 - num2;
+    }
+    this.multiply = () => {
+        return num1 * num2;
+    }
+    this.divide = () => {
+        return num1 / num2;
     }
 };
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,6 +1,21 @@
 import calculator from "../code/calculator";
 
 test('add', () => {
-    const sum = new calculator(1, 2)
+    const sum = new calculator(1, 2);
     expect(sum.add()).toBe(3);
+})
+
+test('subtract', () => {
+    const sum2 = new calculator(4,1);
+    expect(sum2.subtract()).toBe(3);
+})
+
+test('multiply', () => {
+    const sum3 = new calculator(5, 2);
+    expect(sum3.multiply()).toBe(10);
+})
+
+test ('divide', () => {
+    const sum4 = new calculator(20, 4);
+    expect (sum4.divide()).toBe(5);
 })
