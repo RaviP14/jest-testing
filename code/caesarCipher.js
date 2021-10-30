207843b2ce99ecd39168428ba256a23b056c695e
RaviP14 <ravi.patel14@outlook.com>
2021-10-30 13:01:09 +0100

    Added decipher tests and code.

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -35,10 +35,47 @@ const caeser = (() => {
         return cipherString;
     }
 
-    // Decipher fuction?
+    // Decipher fuction
+    function decipher(string, key) {
+        const value = decrypt(string, key);
+
+        return value;
+    }
+
+    function decrypt(string, key) {
+        const array = string.slice().toLowerCase().split('');
+        const decipherArray = [];
+
+        array.forEach(element => {
+            if ((element.charCodeAt(0) >= 97) && (element.charCodeAt(0) <= 122) && (key >= 0 && key <= 25)) {
+                const value = element.charCodeAt(0) - 97;
+                
+                const val2 = 97 + value - key;
+                // If val2 is < (a).
+                if (val2 < 97) {
+                    // To get back to (z).
+                    const val3 = val2 + 123 - 97;
+                    const cipherValue = String.fromCharCode(val3);
+                    decipherArray.push(cipherValue);
+                } else {
+                    const cipherValue = String.fromCharCode(val2);
+                    decipherArray.push(cipherValue);
+                }
+                
+            } else if ((key >= 0 && key <= 25)){
+                decipherArray.push(element);
+            }
+        });
+        
+        const decipherString = decipherArray.join('');
+
+        return decipherString;
+
+    }
 
     return {
         cipher,
+        decipher,
     }
 })();
 
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -19,3 +19,23 @@ test('cipher capital letter test', () => {
 test('cipher puntuation & capital letter test', () => {
     expect(caeser.cipher('HeLlo, agAin?', 5)).toBe('mjqqt, flfns?');
 })
+
+test('decipher1', () => {
+    expect(caeser.decipher('cde', 2)).toBe('abc');
+})
+
+test('wrapping a to z', () => {
+    expect(caeser.decipher('rfc zcqr ctcp', 24)).toBe('the best ever');
+})
+
+test('decipher puntuation test', () => {
+    expect(caeser.decipher('mjqqt, flfns?', 5)).toBe('hello, again?');
+})
+
+test('decipher capital letter test', () => {
+    expect(caeser.decipher('Ftq Iadxp dqOadpe', 12)).toBe('the world records');
+})
+
+test('decipher puntuation & capital letter test', () => {
+    expect(caeser.decipher('MjQqt, flFns?', 5)).toBe('hello, again?');
+})
